ed22b00399ff59165cb97d64a3a9ffa296264a16
Kuba Flis <jakub.flis@performance-media>
2021-12-05 17:32:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const showModal = () => {
+    console.log('Show modal')
+}
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal)
+
+for (let i = 0; i < btnsOpenModal.length; i++){
+    console.log(btnsOpenModal[i].textContent)
+}
+
